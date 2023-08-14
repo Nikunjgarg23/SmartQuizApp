36d8c2a8fd266d780bdfc5b7509aaf28f7f6d34f
prateek <prateeksrivastav703@gmail.com>
2023-08-15 01:54:56 +0530

    adding teacher page

--- views/index.js
+++ views/index.js
@@ -1,10 +1,21 @@
 import express from "express";
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
+
 app.use(express.static("public"));
 app.get("/", function (req, res) {
     res.render("index.ejs");
-
 });
-app.listen("3000",()=>{
+app.get("/teacher", function (req, res) {
+    res.render(__dirname + "/teacher.ejs")
+});
+// app.post("/indexteacher", function (req, res) {
+//     const a=5;
+//     res.send("Your BMI is : " + a);
+// });
+app.listen("3000", () => {
     console.log(`server is running on port 3000`);
 });
