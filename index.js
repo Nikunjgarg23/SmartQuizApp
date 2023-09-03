1d3505e01bfba3d3469ab09d5c2708fb1e74041b
Nikunj <23nikunjgarg@gmail.com>
2023-09-04 01:06:52 +0530

    add

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// import { log } from "console";
-// import express from "express";
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const app = express();
-//import db from "../config/mongoose.js";
-
 const express = require('express');
 const app = express();
 const db = require('./config/mongoose');
@@ -17,58 +8,19 @@ app.set('views','./views');
 app.use(express.urlencoded());
 app.use(express.static('public'));
 
-//const db = require('../config/mongoose');
-// import Teacher from "../models/teacher.js";
-// app.use(express.urlencoded());
 
 
-// app.use(express.static("public"));
 app.use('/',require('./routes'));
-// app.get("/", function (req, res) {
-//     return res.render("index");
-// });
+
+
 app.get("/student", function (req, res) {
     return res.render("student");
 });
 
 
-// app.get("/teacher", function (req, res) {
-//     return res.render("teacher-signin");
-// });
-
-
-// app.get("/teacher/signup", function (req, res) {
-//     return res.render("teacher-signup");
-// });
-
-
 app.get("/assessment", function (req, res) {
     return res.render("assessment");
 });
-
-// app.post('/teacher/create',function(req,res){
-//     if(req.body.password != req.body.confirm_pass){
-//         return res.redirect('back');
-//     }
-//     const find = async()=>{
-//         try{
-//             const user = await Teacher.findOne({email : req.body.email});
-
-//             if(!user){
-//                 const data = new Teacher(req.body);
-//                 data.save();
-//                 return res.redirect('/teacher'); // change to signup page later
-//             }
-//             else{
-//                 return res.redirect('/teacher/signup');
-//             }
-//         }
-//         catch(err){
-//             console.log(err);
-//         }
-//     }
-//     find();
-// });
 
 
 app.listen("3000", () => {
